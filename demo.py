b77b2a9502a901726fc4e58d75136cc98a6c81f1
elahw-zh25 <85200114+elahw-zh25@users.noreply.github.com>
2021-06-04 15:16:37 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,4 +2,4 @@ import MusicClient
 c = MusicClient.Client()
 c.set_uid() # fill this with uin in your cookie
 c.load_cookie("") # fill this with your cookie string on qq music site.
-res = c.get_song_fav_playlist(2339472476, "华语热门.csv")
+res = c.get_song_fav_playlist(2339472476, "华语热门.tsv")
